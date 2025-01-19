96f0df1852664b4c59b07f6aa4517ece948fbdfd
Celso Farias <celso.farias@useblu.com.br>
2025-01-19 00:35:03 -0300

    fix: change mutableListOf per mutableStateListOf

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -72,7 +73,7 @@ fun PaintApp() {
     val coroutineScope = rememberCoroutineScope()
 
     var currentColor by remember { mutableStateOf(Color.Black) }
-    val lines = remember { mutableListOf<Line>() }
+    val lines = remember { mutableStateListOf<Line>() }
     var brushSize by remember { mutableFloatStateOf(10f) }
     var isEraser by remember { mutableStateOf(false) }
 
